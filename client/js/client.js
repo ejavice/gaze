35783556d743cf88d387bb3e787078699bfa9154
Elie Javice <ejavice@me.com>
2013-01-19 10:20:09 -0500

    basically done design

--- client/js/client.js
+++ client/js/client.js
@@ -4,12 +4,12 @@ var data;
 var gorkem;
 Meteor.call("get_data", data, function(error, xmldata) {
 		gorkem = xmldata;
-	console.log(gorkem);
+	//console.log(gorkem);
 	xmlDoc = $.parseXML( gorkem ),
 		$xml = $( xmlDoc ),
 		$url = $xml.find( "result" ).each(function (index) {
 			$final_url = $(this).find("media").find("media_item url");
-				console.log($final_url.text());
+				//console.log($final_url.text());
 			
 		});
 });
@@ -20,6 +20,8 @@ Template.content.newsItem = function() {
 		imageUrl: "http://www.musicnfilms.com/wp-content/uploads/2012/11/Justin-Bieber-photos-songs-list.jpg",
 		articleUrl: "http://www.nyt.com",
 		imageUrl1: "http://photography.naturestocklibrary.com/orca-stock-photo.jpg",
+		imageUrl2: "http://a0.twimg.com/profile_images/1226543249/Justin_Bieber__179__reasonably_small.jpg",
+		imageUrl3: "https://twimg0-a.akamaihd.net/profile_images/2974305624/105d75afc69b5c704c7d8b987a21473d.png",
 		articleName: "Hello Kitty",
 		articleTopic: "Topic Topic"
 	};
@@ -27,19 +29,18 @@ Template.content.newsItem = function() {
 };
 
 Template.content.rendered = function () {
-		var $container = $('#container');
-		$container.imagesLoaded( function(){
-				$container.masonry({
-						itemSelector : '.item',
-						isAnimated : true,
-						columnWidth : 100
-				});
-		});
-};
+	var $container = $('#container');
+	$container.imagesLoaded( function(){
+			$container.masonry({
+					itemSelector : '.item',
+					isAnimated : true,
+					columnWidth: function( containerWidth ) {
+						return containerWidth / 7;
+					}
+			});
+	});
 
-// Template.content.rendered = function () {
-// 	$("#container").isotope({
-// 		itemSelector : '.item',
-// 		layoutMode : 'fitRows'
-// 	});
-// };
+	$(document).scroll(function() {
+		$("#navContainer").css("margin-top",""+$(document).scrollTop()+"px");
+	});
+};
